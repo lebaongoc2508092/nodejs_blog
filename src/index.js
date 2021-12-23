f9d95a1d707dba4b9278bf427e887e01f5824125
lebaongoc250892 <lebaongoc250892@gmail.com>
2021-12-23 23:40:20 +0700

    router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 // HTTP logger
 app.use(morgan('combined'));
@@ -17,12 +19,9 @@ app.set('view engine','hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log('Path:', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
